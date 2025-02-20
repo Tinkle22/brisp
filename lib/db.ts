a4fc8a7103c7e26699474703310258e99f68c6e6
Tinkle22 <104595797+Tinkle22@users.noreply.github.com>
2025-02-20 19:43:04 +0200

    added cert

--- lib/db.ts
+++ lib/db.ts
@@ -1,22 +1,24 @@
 import mysql from 'mysql2/promise';
+import fs from 'fs';
+import path from 'path';
 
-const sslCert = process.env.DATABASE_SSL_CA;
+const sslCertPath = process.env.DATABASE_SSL_CERT;
 
-if (!sslCert) {
-throw new Error("DATABASE_SSL_CA environment variable is not defined.");
+if (!sslCertPath) {
+  throw new Error("DATABASE_SSL_CERT environment variable is not defined.");
 }
 
 const pool = mysql.createPool({
-host: process.env.MYSQL_HOST,
-database: process.env.MYSQL_DATABASE,
-user: process.env.MYSQL_USER,
-password: process.env.MYSQL_PASSWORD,
-ssl: {
-  ca: sslCert, // Directly use the string
-},
-waitForConnections: true,
-connectionLimit: 10,
-queueLimit: 0
+  host: process.env.MYSQL_HOST,
+  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  ssl: {
+    ca: fs.readFileSync(path.resolve(process.cwd(), sslCertPath)),
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 export default pool;
