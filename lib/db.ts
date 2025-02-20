e5182b72bcf228440cc460a097fffbb51656b325
Tinkle22 <104595797+Tinkle22@users.noreply.github.com>
2025-02-20 19:26:00 +0200

    added aiven

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,21 @@
 import mysql from 'mysql2/promise';
+import fs from 'fs';
+import path from 'path';
+
+const sslCertPath = process.env.DATABASE_SSL_CERT;
+
+if (!sslCertPath) {
+  throw new Error("DATABASE_SSL_CERT environment variable is not defined.");
+}
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   database: process.env.MYSQL_DATABASE,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
+  ssl: {
+    ca: fs.readFileSync(path.resolve(process.cwd(), sslCertPath)),
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
